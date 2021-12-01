fed0aeb68bc12240ef61cc79bef4d8ad1974369c
visbm <visbm@mail.ru>
2021-12-01 20:56:13 +0300

    commit docker compose

--- api/main.go
+++ api/main.go
@@ -4,15 +4,31 @@ import (
 	"goReact/webapp"
 	"goReact/webapp/server"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
 
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+
+	}
 	config := &webapp.Config{}
 	config.ReadFromFile("config.yaml")
 
 	s := server.New(config)
+	/*if err := s.Run(viper.GetString("8081"), handlers.InitRoutes()); err != nil{
+
+	}*/
+
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("./")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
