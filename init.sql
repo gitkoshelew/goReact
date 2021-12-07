9f6eb25b13c8cd20b01b0c34060340a5e8c3d2dd
visbm <visbm@mail.ru>
2021-12-07 21:13:18 +0300

    Added init.sql scrypt Adding init.sql scrypt to fill db on initialization. Сorrected typos

--- init.sql
+++ init.sql
@@ -68,3 +68,54 @@ CREATE TABLE IF NOT EXISTS BOOKING
     end_date         DATE NOT NULL ,
     client_notes     TEXT
 );
+
+INSERT INTO ACCOUNT ( Login, Password) VALUES 
+('login1', 'password1'),
+('login2', 'password2'),
+('login3', 'password3'),
+('login4', 'password4'),
+('login5', 'password5'),
+('login6', 'password6');
+
+INSERT INTO USERS (first_name , surname, middle_name, email, date_jf_birth, address, phone, account_id) VALUES 
+('Ivan','Ivanov','Ivanovich','ivan@mail.ru','2000-01-01' ,'Minsk Pr. Nezavisimosti 22-222' ,'+375-29-154-89-33', 1),
+('Petr','Petrov','Petrovich','petr@mail.ru','1999-03-13 ','Minsk Pr. Pobediteley 11-111' ,'+375-29-159-11-78', 2),
+('Maria','Petrova','Evgenievna','liza@mail.ru','2001-11-11' ,'Minsk Pr. Pobediteley 111-111' ,'+375-29-655-99-14', 3),
+('Olga','Oleeva','Vladimirovna','olga@mail.ru','2001-01-01' ,'Minsk ul. Nesterova 1-32' ,'+375-29-675-00-00', 4),
+('Vladzimir','Sakhonchik','Alekseevish','leha@mail.ru','1998-02-11' ,'Minsk Partizanskiy 124-1' ,'+375-29-111-22-33', 5),
+('Mikhail','Valevach','Dmitrievich','miha@mail.ru','1997-01-12' ,'Minsk Pr. Dzerjinskogo 01-01' ,'+375-29-777-55-44', 6);
+
+INSERT INTO HOTEL ( name, address) VALUES 
+('PetsHotel1','ul. Pushkina 12'),
+('PetsHotel2','ul. Sovetskaya 16'),
+('PetsHotel3','ul. Ilimskaya 33');
+
+INSERT INTO ROOM (pet_type, hotel_id) VALUES 
+('Cat', 1),
+('Dog', 2),
+('Cat', 3);
+
+INSERT INTO SEAT (room_id, is_free) VALUES 
+(1, false),
+(2, false),
+(3, false);
+
+INSERT INTO CLIENT ( user_id) VALUES 
+(1),
+(2),
+(3);
+
+INSERT INTO PET (name , type, weignt, dieses, client_id) VALUES 
+('Murzik','cat',5 ,'no dieses' ,1),
+('Barbos','dog',5 ,'1 dieses' ,2),
+('Aliy','dog',5 ,'2 dieses' ,3);
+
+INSERT INTO EMPLOYEE (user_id , position, hotel_id, role) VALUES 
+(4 ,'Podition 1 ',1 ,'role 1' ),
+(5 ,'Podition 3 ',2 ,'role 2' ),
+(6 ,'Podition 3 ',3 ,'role 3' );
+
+INSERT INTO BOOKING (seat_id , pet_id, employee_id, client_id, status, start_date, end_date, client_notes) VALUES 
+(1 ,1 ,1 ,1 ,'In processing', '2021-12-07', '2021-12-27','wash my pet pls twice a day'),
+(2 ,2 ,2 ,2 ,'In work', '2021-12-01', '2021-12-15','feed my pet pls once a week'),
+(3, 3 ,3 ,3 ,'Ended up', '2021-11-26', '2021-12-06',' no comm');
