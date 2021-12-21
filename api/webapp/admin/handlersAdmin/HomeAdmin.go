7fa794381796dcbe95499cf1d9da0b2bcf257e23
visbm <visbm@mail.ru>
2021-12-21 22:24:39 +0300

    fix bags

--- api/webapp/admin/handlersAdmin/HomeAdmin.go
+++ api/webapp/admin/handlersAdmin/HomeAdmin.go
@@ -13,7 +13,6 @@ func HomeAdmin() http.HandlerFunc {
 	db := utils.HandlerDbConnection()
 	return func(w http.ResponseWriter, r *http.Request) {
 		var id int
-		var hashPassword string
 		//var password1 string
 		Login := r.FormValue("login")
 		Password := r.FormValue("password")
@@ -21,27 +20,37 @@ func HomeAdmin() http.HandlerFunc {
 			http.Error(w, "Enter email or password", 400)
 			return
 		}
-		fmt.Println(Login, Password)
+		fmt.Println("ac login and pas 1 : ", Login, Password)
 		account := store.Account{}
-		rowPassword := db.QueryRow("SELECT password FROM ACCOUNT WHERE login = $1 ", Login)
-		err:= rowPassword.Scan(&account.Password)
-		fmt.Println(Login, account.Password)
+		rowPassword := db.QueryRow("SELECT * FROM ACCOUNT WHERE login = $1 ", Login)
+		err:= rowPassword.Scan( &account.AccountID,&account.Login, &account.Password )
+
+		fmt.Println("ac login and pas 2 : " ,account.Login, account.Password)
+
+		hashPassword := account.Password
+		hashPid := account.AccountID
+		hashlogin := account.Login
+		
+		
 		if err !=nil{
 			http.Error(w, "Check your email or password1", 400)
 			return
 		}
 
 		isConfirmed := store.CheckPasswordHash(Password , hashPassword)
+		fmt.Println("ac login and pas 3 : " ,hashlogin, account.Password, hashPid)
+		fmt.Println(isConfirmed)
 		if !isConfirmed {
 			http.Error(w, "Check your email or password2", 401)
 			return
 		}
 
-		row := db.QueryRow("SELECT * FROM ACCOUNT WHERE login = $1 AND password=$2", Login,Password)
+		/*row := db.QueryRow("SELECT * FROM ACCOUNT WHERE login = $1 AND password=$2", Login,Password)
 		err = row.Scan(&id,&Login)
 		if err !=nil{
-			http.Error(w, err.Error(), 400)
-		}
+			http.Error(w, err.Error(), 400)	
+		return
+		}*/
 
 
 		
