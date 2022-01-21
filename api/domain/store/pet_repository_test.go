a13084c691ad199103dbe36d99b92a421a0462e9
visbm <visbm@mail.ru>
2022-01-21 15:11:20 +0300

    repository test creat

---
+++ api/domain/store/pet_repository_test.go
@@ -0,0 +1,61 @@
+package store_test
+
+import (
+	"goReact/domain/model"
+	"goReact/domain/store"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPetRepository_Create(t *testing.T) {
+	s, teardown := store.TestStore(t, host, dbName, user, password, port, sslMode)
+	t.Cleanup(teardown)
+	t.Run("valid", func(t *testing.T) {
+		p, err := s.Pet().Create(model.TestPet())
+		assert.NoError(t, err)
+		assert.NotNil(t, p)
+	})
+}
+
+func TestPetRepository_Delete(t *testing.T) {
+	s, teardown := store.TestStore(t, host, dbName, user, password, port, sslMode)
+	t.Cleanup(teardown)
+	t.Run("invalid id", func(t *testing.T) {
+		id := 2
+		err := s.Pet().Delete(id)
+		assert.Error(t, err)
+	})
+	t.Run("valid id", func(t *testing.T) {
+		p := model.TestPet()
+		_, err := s.Pet().Create(p)
+		err = s.Pet().Delete(p.PetID)
+		assert.NoError(t, err)
+	})
+}
+
+func TestPetRepository_FindByID(t *testing.T) {
+	s, teardown := store.TestStore(t, host, dbName, user, password, port, sslMode)
+	t.Cleanup(teardown)
+	t.Run("invalid id", func(t *testing.T) {
+		id := 2
+		_, err := s.Pet().FindByID(id)
+		assert.Error(t, err)
+	})
+	t.Run("valid id", func(t *testing.T) {
+		p := model.TestPet()
+		p, err := s.Pet().FindByID(p.PetID)
+		assert.NoError(t, err)
+		assert.NotNil(t, p)
+	})
+}
+
+func TestPetRepository_GetAll(t *testing.T) {
+	s, teardown := store.TestStore(t, host, dbName, user, password, port, sslMode)
+	t.Cleanup(teardown)
+	t.Run("invalid id", func(t *testing.T) {
+		p, err := s.Pet().GetAll()
+		assert.NoError(t, err)
+		assert.NotNil(t, p)
+	})
+}
