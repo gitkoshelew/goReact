48c9a595e297b4a2c9f1312fe1cbc53be37a9b7f
iForced <iforce@2ch.hk>
2022-01-21 16:38:57 +0300

    transfer socket.io connection to separate service

--- socket-chat-server/index.js
+++ socket-chat-server/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import sequelize from "./db.js";
 import router from "./src/routes/index.js";
-import { Message } from "./src/models/models.js";
+import { startSocket } from "./src/services/socket.js";
 
 dotenv.config();
 
@@ -29,55 +29,12 @@ const PORT = process.env.CHAT_SERVER_PORT || 5000;
 
 app.use("/api", router);
 
-console.log(process.env.CHAT_SERVER_PORT);
-
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
 
-    let connectedUsers = [];
-
-    const addUser = (userId, socketId, receiverId) => {
-      !connectedUsers.some((user) => user.userId === userId) &&
-        connectedUsers.push({ userId, socketId, receiverId });
-    };
-    const removeUser = (socketId) => {
-      connectedUsers = connectedUsers.filter(
-        (user) => user.socketId !== socketId
-      );
-    };
-    const getUser = (userId) => {
-      return connectedUsers.find((user) => user.userId === userId);
-    };
-
-    io.on("connection", (socket) => {
-      const { userId, receiverId } = socket.handshake.query;
-      addUser(userId, socket.id, receiverId);
-      console.log("Connected users", connectedUsers);
-
-      socket.on(
-        "USER_SEND_MESSAGE",
-        async ({ senderId, receiverId, conversationId, text }) => {
-          const message = await Message.create({
-            senderId,
-            receiverId,
-            conversationId,
-            text,
-          });
-          const targetUser = getUser(receiverId);
-          io.to(socket.id).emit("SERVER_SEND_MESSAGE", message);
-          if (targetUser && targetUser.receiverId === senderId) {
-            io.to(targetUser.socketId).emit("SERVER_SEND_MESSAGE", message);
-          }
-        }
-      );
-
-      socket.on("disconnect", () => {
-        removeUser(socket.id);
-        console.log("Connected users after leave", connectedUsers);
-      });
-    });
+    startSocket(io);
 
     server.listen(PORT, () => console.log(`Server starts on port ${PORT}`));
   } catch (e) {
