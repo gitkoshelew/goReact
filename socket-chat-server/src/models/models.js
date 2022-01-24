d79a41783049da9e088378b8ced26d7dc33f5351
iForced <iforce@2ch.hk>
2022-01-25 00:24:14 +0300

    refactor data types of entities in database models, fix path in socket.js

--- socket-chat-server/src/models/models.js
+++ socket-chat-server/src/models/models.js
@@ -1,33 +1,23 @@
 import sequelize from "../../db.js";
 import { DataTypes } from "sequelize";
 
-export const User = sequelize.define("user", {
-  id: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    defaultValue: DataTypes.UUIDV1,
-  },
-  login: { type: DataTypes.STRING, unique: true, allowNull: false },
-  password: { type: DataTypes.STRING },
-});
-
 export const Message = sequelize.define("message", {
   id: {
     type: DataTypes.UUID,
     primaryKey: true,
     defaultValue: DataTypes.UUIDV1,
   },
-  senderId: { type: DataTypes.UUID, allowNull: false },
-  receiverId: { type: DataTypes.UUID, allowNull: false },
-  conversationId: { type: DataTypes.UUID, allowNull: false },
+  senderId: { type: DataTypes.INTEGER, allowNull: false },
+  receiverId: { type: DataTypes.INTEGER, allowNull: false },
+  conversationId: { type: DataTypes.INTEGER, allowNull: false },
   text: { type: DataTypes.STRING, allowNull: false },
 });
 
 export const Conversation = sequelize.define("conversation", {
   id: {
-    type: DataTypes.UUID,
+    type: DataTypes.INTEGER,
     primaryKey: true,
-    defaultValue: DataTypes.UUIDV1,
+    autoIncrement: true,
   },
-  members: { type: DataTypes.ARRAY(DataTypes.UUID) },
+  members: { type: DataTypes.ARRAY(DataTypes.INTEGER) },
 });
