100d9c664f30af4a736bd99c283617e2389e880f
Max <61512470+Mevial@users.noreply.github.com>
2022-02-01 19:37:59 +0300

    add pagination test numbers and styles

--- front/src/main/ui/pages/Room/Room.tsx
+++ front/src/main/ui/pages/Room/Room.tsx
@@ -12,52 +12,66 @@ import { fetchRoomRequest } from "../../../bll/reducers/RoomPageReducer/roomPage
 const { roomPage } = styles
 
 export const Room = () => {
-    useEffect(() => {
-        dispatch(fetchRoomRequest())
-    }, [])
+    const pages = [1, 2, 3, 4, 5]
     const dispatch = useDispatch();
     const rooms = useSelector((state: AppRootState) => state.RoomPage.rooms)
+    // const totalRoomsCount = useSelector((state: AppRootState) => state.RoomPage.totalRoomsCount)
+    const pageSize = useSelector((state: AppRootState) => state.RoomPage.pageSize)
+    const currentPage = useSelector((state: AppRootState) => state.RoomPage.currentPage)
+
+    useEffect(() => {
+        dispatch(fetchRoomRequest(currentPage, pageSize))
+    }, [])
 
 
     return (
         <>
-            {rooms.length === 0
-                ? (<span
-                    className={styles.textNameWithoutCards}>Sorry, but there are no available rooms at the moment</span>)
-                : (rooms.map((room) => {
-                            const { roomNum, hotelId, roomPhotoUrl, petType } = room
-                            return (
-                                <div key={room.roomId} className={roomPage}>
-                                    <Card sx={{ minWidth: 275, minHeight: 250, maxWidth: 350, maxHeight: 300, padding: 2 }}>
-                                        <CardContent className='Wrapper'>
-                                            <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
-                                                The room number: {roomNum}
-                                            </Typography>
-                                            <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
-                                                The hotel number: {hotelId}
-                                            </Typography>
-                                            <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
-                                                Photo: {roomPhotoUrl}
-                                            </Typography>
-                                            <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
-                                                Pet type: {petType}
-                                            </Typography>
-                                        </CardContent>
-                                        <CardActions style={{ justifyContent: 'center' }}>
+            <div className={styles.pages}>
+                {pages.map((page, index) => <span
+                        key={index}
+                        className={currentPage === page ? `${styles.currentPage}` : `${styles.page}`}
+                        onClick={() => dispatch(fetchRoomRequest(page, pageSize))}
+                    >{page}</span>
+                )}
+            </div>
+            {
+                rooms.length === 0
+                    ? (<span
+                        className={styles.textNameWithoutCards}>Sorry, but there are no available rooms at the moment</span>)
+                    : (rooms.map((room) => {
+                                const { roomNum, hotelId, roomPhotoUrl, petType } = room
+                                return (
+                                    <div key={room.roomId} className={roomPage}>
+                                        <Card sx={{ minWidth: 275, minHeight: 250, maxWidth: 350, maxHeight: 300, padding: 2 }}>
+                                            <CardContent className='Wrapper'>
+                                                <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
+                                                    The room number: {roomNum}
+                                                </Typography>
+                                                <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
+                                                    The hotel number: {hotelId}
+                                                </Typography>
+                                                <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
+                                                    Photo: {roomPhotoUrl}
+                                                </Typography>
+                                                <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
+                                                    Pet type: {petType}
+                                                </Typography>
+                                            </CardContent>
+                                            <CardActions style={{ justifyContent: 'center' }}>
 
-                                            <Button variant="contained" size="small" color={"success"}
-                                                    onClick={() => {
-                                                        alert('Booking!!')
-                                                    }}>Booking</Button>
-                                        </CardActions>
-                                    </Card>
-                                </div>
+                                                <Button variant="contained" size="small" color={"success"}
+                                                        onClick={() => {
+                                                            alert('Booking!!')
+                                                        }}>Booking</Button>
+                                            </CardActions>
+                                        </Card>
+                                    </div>
 
-                            );
+                                );
 
-                        }
+                            }
+                        )
                     )
-                )
             }
         </>
     )
