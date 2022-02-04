b33dbf92219545020b86113c66d67ddcb97eeb12
Kazbek <kazbeksungurov@gmail.com>
2022-02-04 22:29:47 +0300

    Seats was updated, modelSeat was changed, added migrations

--- api/domain/store/room_repository.go
+++ api/domain/store/room_repository.go
@@ -127,3 +127,18 @@ func (r *RoomRepository) GetAllPagination(p *pagination.Page) (*[]model.Room, er
 	}
 	return &rooms, nil
 }
+
+// RoomFromDTO ...
+func (r *RoomRepository) RoomFromDTO(dto *model.RoomDTO) (*model.Room, error) {
+	hotel, err := r.Store.HotelRepository.FindByID(dto.HotelID)
+	if err != nil {
+		return nil, err
+	}
+	return &model.Room{
+		RoomID:       dto.RoomID,
+		RoomNumber:   dto.RoomNumber,
+		PetType:      dto.PetType,
+		Hotel:        *hotel,
+		RoomPhotoURL: dto.RoomPhotoURL,
+	}, nil
+}
