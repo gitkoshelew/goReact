3a2d5ea77e11d4ccb03c149ee59d287837adc208
Max <61512470+Mevial@users.noreply.github.com>
2022-02-17 20:15:06 +0300

    add docker, dockerignore, dockerfile, wait-for-postgres
    --update docker-compose file, .env

--- nest-payment-server/src/main.ts
+++ nest-payment-server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 5002;
+  const PORT = process.env.NEST_SERVER_PORT || 5002;
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Backend')
