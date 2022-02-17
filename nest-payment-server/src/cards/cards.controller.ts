c5a1764b6cb985129ffadaa9575fd1bdb1764395
Max <61512470+Mevial@users.noreply.github.com>
2022-02-17 19:11:45 +0300

    add controller, service, bcrypt .env

--- nest-payment-server/src/cards/cards.controller.ts
+++ nest-payment-server/src/cards/cards.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCardDto } from './dto/create-card.dto';
+import { CardsService } from './cards.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CardsModel } from './cards.model';
 
-@Controller('cards')
-export class CardsController {}
+@ApiTags('Cards')
+@Controller('api/cards')
+export class CardsController {
+  constructor(private cardsService: CardsService) {}
+  @ApiOperation({ summary: 'Create card' })
+  @ApiResponse({ status: 200, type: CardsModel })
+  @Post()
+  create(@Body() cardDto: CreateCardDto) {
+    return this.cardsService.createCard(cardDto);
+  }
+  @ApiOperation({ summary: 'Get all cards' })
+  @ApiResponse({ status: 200, type: [CardsModel] })
+  @Get()
+  getAll() {
+    return this.cardsService.getAllCards();
+  }
+}
--- nest-payment-server/src/cards/cards.module.ts
+++ nest-payment-server/src/cards/cards.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CardsService } from './cards.service';
 import { CardsController } from './cards.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CardsModel } from './cards.model';
 
 @Module({
   providers: [CardsService],
   controllers: [CardsController],
+  imports: [SequelizeModule.forFeature([CardsModel])],
 })
 export class CardsModule {}
