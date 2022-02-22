3bceede618b269049ea3b11bd3afd9679f1c570f
Max <61512470+Mevial@users.noreply.github.com>
2022-02-22 17:11:45 +0300

    connect global pipe

--- nest-payment-server/src/main.ts
+++ nest-payment-server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from './pipes/validation.pipe';
 
 async function bootstrap() {
   const PORT = process.env.NEST_SERVER_PORT || 5002;
@@ -13,6 +14,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 
