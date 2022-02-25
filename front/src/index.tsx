a38732a64c347d45c633f379fabbe36737112598
Max <61512470+Mevial@users.noreply.github.com>
2022-02-25 19:51:13 +0300

    add service-worker, registation

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -5,6 +5,7 @@ import App from './main/ui/pages/App'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './main/bll/store/store'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <Router>
@@ -14,3 +15,8 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
