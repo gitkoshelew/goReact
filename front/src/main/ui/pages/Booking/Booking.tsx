b29cef23261b7973c8f91ba335da261aa82818b8
Max <61512470+Mevial@users.noreply.github.com>
2022-03-01 18:51:38 +0300

    fix spaces and prettier

--- front/src/main/ui/pages/Booking/Booking.tsx
+++ front/src/main/ui/pages/Booking/Booking.tsx
@@ -15,93 +15,92 @@ import { BookingRoomPickSaga } from '../../../bll/reducers/BookingRoomsPickReduc
 const { bookingPage, bookingForm, bookingProcess, bookingCalendar, uploadOrderedRoomsBlock } = s
 
 type FormValues = {
-    firstName: string
-    lastName: string
-    email: string
+  firstName: string
+  lastName: string
+  email: string
 }
 
 export const Booking = () => {
-    const validate = (values: FormValues) => {
-        const errors: FormikErrors<FormValues> = {}
-        if (!values.firstName) {
-            errors.firstName = 'Required field'
-        } else if (values.firstName.length > 15) {
-            errors.firstName = 'Must be 15 characters or less'
-        }
-        if (!values.lastName) {
-            errors.lastName = 'Required field'
-        } else if (values.lastName.length > 15) {
-            errors.lastName = 'Must be 15 characters or less'
-        }
-        if (!values.email) {
-            errors.email = 'Required field'
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-            errors.email = 'Invalid email address'
-        }
-        return errors
+  const validate = (values: FormValues) => {
+    const errors: FormikErrors<FormValues> = {}
+    if (!values.firstName) {
+      errors.firstName = 'Required field'
+    } else if (values.firstName.length > 15) {
+      errors.firstName = 'Must be 15 characters or less'
     }
+    if (!values.lastName) {
+      errors.lastName = 'Required field'
+    } else if (values.lastName.length > 15) {
+      errors.lastName = 'Must be 15 characters or less'
+    }
+    if (!values.email) {
+      errors.email = 'Required field'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address'
+    }
+    return errors
+  }
 
-    const formik = useFormik({
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            email: '',
-        },
-        validate,
-        onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2))
-        },
-    })
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
 
-    const loadingStatus = useSelector((state: AppRootState) => state.BookingRoomPick.loadingStatus)
+  const loadingStatus = useSelector((state: AppRootState) => state.BookingRoomPick.loadingStatus)
 
-    const ErrorView = loadingStatus === 'error' ? <div>error</div> : <BookingCalendar/>
-    const correctView = loadingStatus === 'loading' ? <Preloader/> : ErrorView
-    const dispatch = useAppDispatch()
+  const ErrorView = loadingStatus === 'error' ? <div>error</div> : <BookingCalendar />
+  const correctView = loadingStatus === 'loading' ? <Preloader /> : ErrorView
+  const dispatch = useAppDispatch()
 
-    useEffect(() => {
-        dispatch(BookingRoomPickSaga())
-    }, [])
+  useEffect(() => {
+    dispatch(BookingRoomPickSaga())
+  }, [])
 
-    const progress = useSelector((state: AppRootState) => state.BookingRegForm.progress)
-    const actualDay = useSelector((state: AppRootState) => state.BookingRoomPick.actualDay)
-    const isRentArr = useSelector((state: AppRootState) => state.BookingRoomPick.isRent)
-    const orderedRoomBasket = useSelector((state: AppRootState) => state.BookingRoomPick.orderedRoomBasket)
+  const progress = useSelector((state: AppRootState) => state.BookingRegForm.progress)
+  const actualDay = useSelector((state: AppRootState) => state.BookingRoomPick.actualDay)
+  const isRentArr = useSelector((state: AppRootState) => state.BookingRoomPick.isRent)
+  const orderedRoomBasket = useSelector((state: AppRootState) => state.BookingRoomPick.orderedRoomBasket)
 
-    const isActiveBtn = progress === 'uploaded' && orderedRoomBasket.length !== 0
+  const isActiveBtn = progress === 'uploaded' && orderedRoomBasket.length !== 0
 
-    const roomIndicate = useMemo(() => {
-        const newActualDay = isRentArr && isRentArr.find((t) => t.id === actualDay)
-        return newActualDay ? newActualDay : null
-    }, [actualDay, isRentArr])
+  const roomIndicate = useMemo(() => {
+    const newActualDay = isRentArr && isRentArr.find((t) => t.id === actualDay)
+    return newActualDay ? newActualDay : null
+  }, [actualDay, isRentArr])
 
-    return (
-        <div className="bookingContainer">
-            <form onSubmit={formik.handleSubmit}>
-                <div className={bookingPage}>
-                    <TitlePageTextBlock mainTextMess={'Book room for pet'} isWithLink={false}/>
-                    <div className={bookingProcess}>
-                        <div className={bookingForm}>
-                            <BookingRegForm formik={formik}/>
-                        </div>
-                        <div className={bookingCalendar}>
-                            {correctView}
-                            {roomIndicate && (
-                                <BookingRoom
-                                    dayId={roomIndicate.id}
-                                    firstRoom={roomIndicate.firstRoom}
-                                    secondRoom={roomIndicate.secondRoom}
-                                />
-                            )}
-                        </div>
-                    </div>
-                    <div className={uploadOrderedRoomsBlock}>
-                        {orderedRoomBasket.length !== 0 && <SelectedToOrderRoom orderedRoomBasket={orderedRoomBasket}/>}
-                        <Button view={'upload'} disabled={!isActiveBtn}/>
-                    </div>
-                </div>
-            </form>
+  return (
+    <div className="bookingContainer">
+      <form onSubmit={formik.handleSubmit}>
+        <div className={bookingPage}>
+          <TitlePageTextBlock mainTextMess={'Book room for pet'} isWithLink={false} />
+          <div className={bookingProcess}>
+            <div className={bookingForm}>
+              <BookingRegForm formik={formik} />
+            </div>
+            <div className={bookingCalendar}>
+              {correctView}
+              {roomIndicate && (
+                <BookingRoom
+                  dayId={roomIndicate.id}
+                  firstRoom={roomIndicate.firstRoom}
+                  secondRoom={roomIndicate.secondRoom}
+                />
+              )}
+            </div>
+          </div>
+          <div className={uploadOrderedRoomsBlock}>
+            {orderedRoomBasket.length !== 0 && <SelectedToOrderRoom orderedRoomBasket={orderedRoomBasket} />}
+            <Button view={'upload'} disabled={!isActiveBtn} />
+          </div>
         </div>
-
-    )
+      </form>
+    </div>
+  )
 }
