d93453ff6a1ed183963023d4b510bf3415f04cfc
visbm <visbm@mail.ru>
2022-03-03 15:19:37 +0300

    added commets to view data struct

--- rest/admin/pkg/viewData/viewData.go
+++ rest/admin/pkg/viewData/viewData.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
+//ViewData struct needs to show permitted data in templates
 type ViewData struct {
 	ResponseWriter http.ResponseWriter
 	Request        *http.Request
 }
 
+//HasPermission determines have employee has access to certain data
 func (vd ViewData) HasPermission(name string) bool {
 	err := session.CheckRigths(vd.ResponseWriter, vd.Request, name)
 	return err == nil
