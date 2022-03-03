391b29ae045f88e24ba0dc5993dc66ad6bc7f7d1
Maksim Alenkau <61512470+Mevial@users.noreply.github.com>
2022-03-03 19:31:04 +0300

    Front/#147  add responsive booking and fix bug(ticket #126) on big screen (#168)
    * add layout bookingContainer
    
    * fix layout to container
    
    * add margin top and media
    
    * fix text title and add media 575
    
    * fix calendar, add media, fix big screen
    
    * fix bug with position
    
    --if you were offline the message was fixed
    
    * fix text upload
    
    * delete unused property (container)
    
    * fix spaces
    
    * fix spaces and prettier
    
    * fix spaces and prettier
    
    * Update front/src/index.scss
    
    Co-authored-by: Ann40a <ann40a@yandex.ru>
    
    * moved the property to custom class
    
    * fix size input on big screen
    
    * add tooltip container, delete magic number
    
    * move lower calendarTooltip
    
    * delete tooltipContainer
    
    * fix margin top to button
    
    * fix specificity
    
    * add a container and move logic with the lib to the component
    
    * fix bug form error
    
    Co-authored-by: Ann40a <ann40a@yandex.ru>

--- front/src/main/ui/pages/Booking/BookingRegForm/ImgUploadComponent/ImgUpload.tsx
+++ front/src/main/ui/pages/Booking/BookingRegForm/ImgUploadComponent/ImgUpload.tsx
@@ -1,28 +1,13 @@
-import s from './ImgUpload.module.scss'
-import { FileUploader } from 'react-drag-drop-files'
 import { ProgressType } from '../../../../../bll/reducers/BookingRegFormReducer/BookingRegForm-reducer'
-import { useAppDispatch } from '../../../../../bll/store/store'
-import { BookingUploadPetImg } from '../../../../../bll/reducers/BookingRegFormReducer/BookindRegForm-saga'
+import { DragDropFiles } from './DragDropFiles'
+import s from './ImgUpload.module.scss'
 
-const { uploadTablet, customTablet } = s
-const fileTypes = ['JPG', 'PNG', 'GIF', 'JPEG']
+const { uploadTablet } = s
 
 type ImgUploadPropsType = {
   progress: ProgressType
 }
 
 export const ImgUpload = ({ progress }: ImgUploadPropsType) => {
-  const dispatch = useAppDispatch()
-
-  const handleChange = (file: File) => {
-    dispatch(BookingUploadPetImg(file))
-  }
-
-  return (
-    <div className={uploadTablet}>
-      {progress === 'getUpload' && (
-        <FileUploader maxSize={3} classes={customTablet} handleChange={handleChange} name="file" types={fileTypes} />
-      )}
-    </div>
-  )
+  return <div className={uploadTablet}>{progress === 'getUpload' && <DragDropFiles />}</div>
 }
