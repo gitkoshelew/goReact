b47f970f35ef633865cf1edf1bd9d00a8d29f5d7
Max <61512470+Mevial@users.noreply.github.com>
2022-03-09 14:18:31 +0300

    fix cors error to socket, notification

--- notifications-producer/index.js
+++ notifications-producer/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     credential: true,
   })
 );
--- socket-chat-server/index.js
+++ socket-chat-server/index.js
@@ -14,13 +14,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:3001"],
   },
 });
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     credential: true,
   })
 );
