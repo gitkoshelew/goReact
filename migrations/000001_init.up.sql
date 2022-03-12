d1c7754c60656958eb24a676027623e8414b7a63
Kazbek Sungurov <kazbeksungurov@gmail.com>
2022-03-12 17:51:51 +0300

    fix migrations

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS ACCOUNT 
 (   id          serial PRIMARY key , 
-    Login       TEXT NOT NULL DEFAULT '' ,
-    Password    TEXT NOT NULL DEFAULT '' 
+    Login       TEXT NOT NULL ,
+    Password    TEXT NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS USERS 
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS USERS
     middle_name     CHARACTER VARYING(30) ,
     email           CHARACTER VARYING(30) ,
     date_of_birth   DATE NOT NULL ,
-    address         TEXT NOT NULL DEFAULT '' ,
+    address         TEXT NOT NULL ,
     phone           CHARACTER VARYING(30) NOT NULL ,
     account_id      INTEGER REFERENCES ACCOUNT(id) ON DELETE CASCADE NOT NULL 
 );
@@ -19,13 +19,13 @@ CREATE TABLE IF NOT EXISTS USERS
 CREATE TABLE IF NOT EXISTS HOTEL 
 (   id              serial PRIMARY key, 
     name            CHARACTER VARYING(30) NOT NULL ,
-    address         TEXT NOT NULL DEFAULT '' 
+    address         TEXT NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS ROOM 
 (   id          serial PRIMARY key, 
     number      CHARACTER VARYING(30) NOT NULL ,
-    pet_type    TEXT NOT NULL DEFAULT '' ,
+    pet_type    TEXT NOT NULL ,
     hotel_id    INTEGER REFERENCES HOTEL(id) ON DELETE CASCADE 
 );
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS SEAT
 (   id           serial PRIMARY key, 
     room_id      INTEGER REFERENCES ROOM(id) ON DELETE CASCADE NOT NULL ,
     is_free      BOOLEAN NOT NULL,
-    description  TEXT NOT NULL DEFAULT ''
+    description  TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS CLIENT 
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS PET
     name         CHARACTER VARYING(30) NOT NULL ,
     type         CHARACTER VARYING(30) NOT NULL ,
     weignt       SMALLINT NOT NULL ,
-    dieses       NOT NULL DEFAULT '',
+    dieses       TEXT,
     client_id    INTEGER REFERENCES CLIENT(id) ON DELETE CASCADE NOT NULL 
 );
 
@@ -54,8 +54,8 @@ CREATE TABLE IF NOT EXISTS EMPLOYEE
 (   id          serial PRIMARY key ,
     user_id     INTEGER REFERENCES USERS(id) ON DELETE CASCADE NOT NULL ,
     hotel_id    INTEGER REFERENCES HOTEL(id) ON DELETE CASCADE ,
-    position    TEXT NOT NULL DEFAULT '' ,
-    role        TEXT NOT NULL DEFAULT '' 
+    position    TEXT NOT NULL ,
+    role        TEXT NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS BOOKING 
@@ -63,8 +63,8 @@ CREATE TABLE IF NOT EXISTS BOOKING
     seat_id          INTEGER REFERENCES SEAT(id) ON DELETE CASCADE NOT NULL ,
     pet_id           INTEGER REFERENCES PET(id) ON DELETE CASCADE NOT NULL ,
     employee_id      INTEGER REFERENCES EMPLOYEE(id) ON DELETE CASCADE NOT NULL  ,
-    status           TEXT NOT NULL DEFAULT '',
+    status           TEXT ,
     start_date       DATE NOT NULL ,
     end_date         DATE NOT NULL ,
-    client_notes     TEXT NOT NULL DEFAULT ''
+    client_notes     TEXT
 );
