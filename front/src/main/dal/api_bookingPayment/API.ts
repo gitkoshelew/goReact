cd57a51421a7e9d1a76cc08b8e9e62bb46ab012e
Max <61512470+Mevial@users.noreply.github.com>
2022-03-20 22:37:26 +0300

    fix process env

--- front/src/main/dal/api_bookingPayment/API.ts
+++ front/src/main/dal/api_bookingPayment/API.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-// export const API_BOOKING_URL = process.env.NEST_APP_API_PAYMENT_LINK
-export const API_BOOKING_URL = 'http://localhost:5003/'
+export const API_BOOKING_URL = process.env.REACT_APP_API_PAYMENT_LINK
 
 export const apiBookingPayment = axios.create({
   withCredentials: true,
