6e376dc01416888708884431919d39f4f99e840a
Max <61512470+Mevial@users.noreply.github.com>
2022-04-02 23:30:36 +0300

    delete unused search input (rentFrom rentTo)

--- front/src/main/ui/pages/Booking/BookingSearchForm/BookingSearchForm.tsx
+++ front/src/main/ui/pages/Booking/BookingSearchForm/BookingSearchForm.tsx
@@ -56,36 +56,6 @@ export const BookingSearchForm = ({ formik }: BookingSearchFormType) => {
         {formik.errors.petType && formik.touched.petType ? (
           <div className={errorMsg}>{formik.errors.petType}</div>
         ) : null}
-
-        <div className={formik.errors.rentFrom && formik.touched.rentFrom ? errorInputInfo : inputInfo}>
-          Rent form
-          <input
-            className={formik.errors.rentFrom && formik.touched.rentFrom ? errorInputInfoInput : inputInfoInput}
-            id={'rentFrom'}
-            onBlur={formik.handleBlur}
-            value={formik.values.rentFrom}
-            onChange={formik.handleChange}
-            type="date"
-            placeholder={'dd/mm/yyyy'}
-          />
-        </div>
-        {formik.errors.rentFrom && formik.touched.rentFrom ? (
-          <div className={errorMsg}>{formik.errors.rentFrom}</div>
-        ) : null}
-
-        <div className={formik.errors.rentTo && formik.touched.rentTo ? errorInputInfo : inputInfo}>
-          Rent To:
-          <input
-            className={formik.errors.rentTo && formik.touched.rentTo ? errorInputInfoInput : inputInfoInput}
-            id={'rentTo'}
-            onBlur={formik.handleBlur}
-            value={formik.values.rentTo}
-            onChange={formik.handleChange}
-            type="date"
-            placeholder={'dd/mm/yyyy'}
-          />
-        </div>
-        {formik.errors.rentTo && formik.touched.rentTo ? <div className={errorMsg}>{formik.errors.rentTo}</div> : null}
       </div>
     </div>
   )
