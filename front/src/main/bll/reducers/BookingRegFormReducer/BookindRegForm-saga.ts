c15d514d72940aba80e6048486530bb1bf413e66
Max <61512470+Mevial@users.noreply.github.com>
2022-04-13 20:02:20 +0300

    delete mock logic, add new response and save him

--- front/src/main/bll/reducers/BookingRegFormReducer/BookindRegForm-saga.ts
+++ front/src/main/bll/reducers/BookingRegFormReducer/BookindRegForm-saga.ts
@@ -1,49 +1,18 @@
-import { changePhotoUrl, changeProgressStatus, setBookingPhoto } from './BookingRegForm-reducer'
+import { changePhotoUrl, changeProgressStatus } from './BookingRegForm-reducer'
 import { put, call } from 'redux-saga/effects'
 import { AxiosResponse } from 'axios'
-import {
-  BookingInfoPhoto,
-  BookingRegFormAPI,
-  FetchBookingPhotoResponse,
-} from '../../../dal/api_bookingPhoto/BookingService'
-
-const dataGivers = async (data: Blob | MediaSource) => {
-  const result: Blob | MediaSource = await new Promise<Blob | MediaSource>((res) => {
-    setTimeout(() => {
-      res(data)
-    }, 1000)
-  })
-  const correctImg = Object.assign(result, {
-    preview: URL.createObjectURL(result),
-  })
-
-  return correctImg.preview
-}
+import { FilesAPI } from '../../../dal/api_File/FileService'
 
 export function* BookingUploadPetImgSagaWorker(action: BookingUploadPetImgType) {
   yield put(changeProgressStatus({ newStatus: 'uploading' }))
-  const file: string = yield call(dataGivers, action.file)
-  yield put(changePhotoUrl({ newPhotoUrl: file }))
-  // const bookingPhotoResponse: AxiosResponse<FetchBookingPhotoResponse> = yield call(
-  //   BookingRegFormAPI.createBookingPhoto,
-  //   action.newPhotoInfo,
-  //   action.file
-  // )
-  // yield put(
-  //   setBookingPhoto({
-  //     bookingPhoto: bookingPhotoResponse.data,
-  //     bookingInfo: {
-  //       type: 'user',
-  //       ownerId: 1,
-  //     },
-  //   })
-  // )
+  const bookingPhotoResponse: AxiosResponse = yield call(FilesAPI.uploadFile, action.file)
+  yield put(changePhotoUrl({ newPhotoUrl: bookingPhotoResponse.data }))
+
   yield put(changeProgressStatus({ newStatus: 'uploaded' }))
 }
 
-export const BookingUploadPetImg = (file: File, newPhotoInfo: BookingInfoPhoto) => ({
+export const BookingUploadPetImg = (file: File) => ({
   type: 'BOOKING_REG_FORM/BOOKING_PET_IMG_UPLOAD',
   file,
-  newPhotoInfo,
 })
 type BookingUploadPetImgType = ReturnType<typeof BookingUploadPetImg>
