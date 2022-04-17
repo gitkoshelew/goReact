3c6b8ef71287e6006aaab73cef0f886b0263112f
visbm <visbm@mail.ru>
2022-04-17 17:47:46 +0300

    fix

--- api/webapp/server/handler/middleware/linkedinoauth2/linkedInLogin.go
+++ api/webapp/server/handler/middleware/linkedinoauth2/linkedInLogin.go
@@ -24,11 +24,9 @@ func LinkedInLogin(s *store.Store) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 		link := conf.AuthCodeURL("state")
-		s.Logger.Info("link", link)
 
 		http.Redirect(w, r, link, http.StatusFound)
 
 	}
 }
 
-//https://www.linkedin.com/oauth/v2/authorization?client_id=781ygi34xfb27y&redirect_uri=http%!A(string=&scope=r_liteprofile%20r_emailaddress%20)%!F(MISSING)%!F(MISSING)localhost%!A(MISSING)8080%!F(MISSING)api%!F(MISSING)linkedinlogin%!F(MISSING)re&response_type=code&state=state
