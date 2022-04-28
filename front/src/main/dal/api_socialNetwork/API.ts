98d11bb50764e55f9addc8b9a7318bf5ef908cd0
Max <61512470+Mevial@users.noreply.github.com>
2022-04-28 13:29:45 +0300

    delete unused reducer, saga, api, service

--- front/src/main/dal/api_socialNetwork/API.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const API_BOOKING_URL = process.env.REACT_APP_API_SOCIAL_AUTH_LINK
-
-export const apiAuthSocialNetwork = axios.create({
-  withCredentials: true,
-  baseURL: API_BOOKING_URL,
-})
