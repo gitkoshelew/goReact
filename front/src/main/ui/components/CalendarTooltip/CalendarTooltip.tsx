fbe5e730d774e039cba3879b46290fe4303e2a22
Max <61512470+Mevial@users.noreply.github.com>
2022-04-30 16:42:01 +0300

    add filterDay, and change view with number seat

--- front/src/main/ui/components/CalendarTooltip/CalendarTooltip.tsx
+++ front/src/main/ui/components/CalendarTooltip/CalendarTooltip.tsx
@@ -1,41 +1,32 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import s from './CalendarTooltip.module.scss'
 import { useSelector } from 'react-redux'
-import { AppRootState, useAppDispatch } from '../../../bll/store/store'
+import { AppRootState } from '../../../bll/store/store'
 import moment from 'moment'
-import { seatsSearchDay } from '../../../bll/reducers/SeatsReducer/seats-reducer'
+import { SeatResponse } from '../../../dal/api_client/SeatsService'
 
 type PropsType = {
   tooltipDate: Date | null
 }
 
 export const CalendarTooltip = React.memo(({ tooltipDate }: PropsType) => {
-  const dispatch = useAppDispatch()
+  const day = moment(tooltipDate).format('MM DD YYYY')
 
-  const strDate = tooltipDate ? tooltipDate.toString().split(' ') : ''
-
-  const day = Number(strDate[2])
-  useEffect(() => {
-    dispatch(seatsSearchDay({ searchDay: day }))
-  }, [day])
-
-  console.log(day)
-  const currentPickDay = useSelector((state: AppRootState) => state.Seats.searchDay)
-  const seatSearchData = useSelector((state: AppRootState) =>
-    state.Seats.seatsSearch[currentPickDay].seatIds.toString().split(' ')
-  )
-
-  console.log(seatSearchData)
-  console.log(currentPickDay)
+  let filterDay = useSelector<AppRootState, SeatResponse[]>((state: AppRootState) => state.Seats.seatsSearch)[
+    day as unknown as number
+  ]?.seatIds.join(',')
 
   return (
     <div className={s.tooltipData}>
       <h3 className={s.tooltipTitle}>Available seats on {moment(tooltipDate).format('MM/DD/YY')}:</h3>
-
-      <p className={s.tooltipText}>
-        We have available seat in room <span className={s.tooltipTextMark}>{seatSearchData}</span> for your{' '}
-        <span className={s.tooltipTextMark}>pet</span>
-      </p>
+      {filterDay ? (
+        <p className={s.tooltipText}>
+          We have available seat in room <span className={s.tooltipTextMark}>{filterDay}</span> for your{' '}
+          <span className={s.tooltipTextMark}>pet</span>
+        </p>
+      ) : (
+        <p className={s.tooltipText}>Sorry, but there are no available rooms for this date</p>
+      )}
     </div>
   )
 })
